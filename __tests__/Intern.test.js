e03ce478cb99ea633883034802a572cecc32b5c5
Jonathan Cushing <firex42069@gmail.com>
2022-05-07 17:41:03 -0400

    intern test; manager prompts

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,2 +1,12 @@
 const Employee = require("../lib/Employee.js");
 const Intern = require("../lib/Intern.js");
+
+test("creates an intern object", () => {
+    const intern = new Intern("Jonathan", "2", "notarealemail@gmail.com", "The University of Skill Issue");
+
+    expect(intern.name).toBe("Jonathan");
+    expect(intern.id).toBe("2");
+    expect(intern.email).toBe("notarealemail@gmail.com");
+    expect(intern.school).toBe("The University of Skill Issue");
+    expect(intern.getRole()).toBe("Intern");
+});
--- index.js
+++ index.js
@@ -1,8 +1,110 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
+const Engineer = require("./lib/Engineer.js");
+const Intern = require("./lib/Intern.js");
+const Manager = require("./lib/Manager.js");
+const employeeArray = [];
 
+
+function theAwakening() {
+    this.manager = new Manager();
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "Enter the team manager's name",
+            validate: input => {
+                if(input) {
+                    return true;
+                } else {
+                    console.log("dewit");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "Enter the team manager's employee ID",
+            validate: input => {
+                if(input) {
+                    return true;
+                } else {
+                    console.log("dewit");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter the team manager's name",
+            validate: function(input) {
+                if(!input.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+                    if(input) {
+                        console.log("Invalid email address entered. PLease try again");
+                    } else {
+                        console.log("dewit");
+                    }
+                    return false;
+                }
+                return true;
+            }
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter the team manager's office number",
+            validate: input => {
+                if(input) {
+                    return true;
+                } else {
+                    console.log("dewit");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "list",
+            name: "addMember",
+            message: "Time to introduce additional members to your team! Select the type of member you want to add below",
+            choices: ["Engineer", "Intern", "Finish build"]
+        }
+    ]).then(({name, id, email, officeNumber, addMember}) => {
+            this.manager.name = name;
+            this.manager.id = id;
+            this.manager.email = email;
+            this.manager.officeNumber = officeNumber;
+
+            employeeArray.push(this.manager);
+
+            console.log(employeeArray);
+
+            switch(addMember) {
+                case "Engineer":
+                    return addEngineer();
+                case "Intern":
+                    return addIntern();
+                case "Finish build":
+                    return theEnd();
+            }
+    })
+}
+
+function addEngineer() {
+    console.log("engineer option");
+}
+
+function addIntern() {
+    console.log("Intern option");
+}
+
+function theEnd() {
+    console.log("Too fast, too soon");
+}
 /*
-prompts: (r=required, t-text, l=list)
+prompts: (r=required, t=text, l=list)
 r-t-manager-name
 r-t-employee-id
 r-t-email
@@ -23,3 +125,5 @@ i-l-add-employee
 
 default-end-prompts
 */
+
+theAwakening();
