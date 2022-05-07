6661313c33b0c5b7e4317578cc543d04ce4a42fc
Jonathan Cushing <firex42069@gmail.com>
2022-05-07 13:42:27 -0400

    take care of engineer; refine employee a bit

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,20 +1,18 @@
 const Employee = require("../lib/Employee.js");
 
 test("creates a new employee", () => {
-    const employee = new Employee("Jonathan", "1", "notarealemail@google.com");
+    const employee = new Employee("Jonathan", "1", "NotARealEmail@gmail.com");
 
     expect(employee.name).toBe("Jonathan");
     expect(employee.id).toBe("1");
-    expect(employee.email).toBe("notarealemail@google.com");
+    expect(employee.email).toBe("notarealemail@gmail.com");
     expect(employee.getRole()).toBe("Employee");
 });
 
 test("email validation", () => {
-    const employee1 = new Employee("Jonathan", "1", "notarealemail@google.com");
+    const employee1 = new Employee("Jonathan", "1", "NotARealEmail@gmail.com");
+    expect(employee1.getEmail()).toBe("notarealemail@gmail.com");
 
-    expect(employee1.getEmail()).toBe("notarealemail@google.com");
-
-    const employee2 = new Employee("Jonathan", "1", "totallyarealemail.google.com");
-
+    const employee2 = new Employee("Jonathan", "1", "totallyarealemail.gmail.com");
     expect(employee2.getEmail()).toBeFalsy();
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,2 +1,26 @@
 const Employee = require("../lib/Employee.js");
 const Engineer = require("../lib/Engineer.js");
+
+test("creates an engineer object", () => {
+    const engineer = new Engineer("Jonathan", "2", "notarealemail@gmail.com", "JEC6789");
+
+    expect(engineer.name).toBe("Jonathan");
+    expect(engineer.id).toBe("2");
+    expect(engineer.email).toBe("notarealemail@gmail.com");
+    expect(engineer.github).toBe("JEC6789");
+    expect(engineer.getRole()).toBe("Engineer");
+});
+
+test("github username validation", () => {
+    const engineer1 = new Engineer("Jonathan", "2", "notarealemail@gmail.com", "JEC6789");
+    expect(engineer1.getGithub()).toBe("JEC6789");
+
+    const engineer2 = new Engineer("Jonathan", "2", "notarealemail@gmail.com", "-Negativland-");
+    expect(engineer2.getGithub()).toBeFalsy();
+
+    const engineer3 = new Engineer("Jonathan", "2", "notarealemail@gmail.com", "MyUsernameIsGonnaBeOver40CharactersLongAndThereIsNothingYouCanDoToStopMe");
+    expect(engineer3.getGithub()).toBeFalsy();
+
+    const engineer4 = new Engineer("Jonathan", "2", "notarealemail@gmail.com", "xXx_$$$_YoloSwag420BlazeIt_$$$_xXx");
+    expect(engineer4.getGithub()).toBeFalsy();
+});
