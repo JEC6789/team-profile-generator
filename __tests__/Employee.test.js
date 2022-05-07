b69221e38a64a4e463a80cf23c98d331097bdfcc
Jonathan Cushing <firex42069@gmail.com>
2022-05-07 12:45:18 -0400

    the binge begins - getEmail function; begin employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,20 @@
 const Employee = require("../lib/Employee.js");
+
+test("creates a new employee", () => {
+    const employee = new Employee("Jonathan", "1", "notarealemail@google.com");
+
+    expect(employee.name).toBe("Jonathan");
+    expect(employee.id).toBe("1");
+    expect(employee.email).toBe("notarealemail@google.com");
+    expect(employee.getRole()).toBe("Employee");
+});
+
+test("email validation", () => {
+    const employee1 = new Employee("Jonathan", "1", "notarealemail@google.com");
+
+    expect(employee1.getEmail()).toBe("notarealemail@google.com");
+
+    const employee2 = new Employee("Jonathan", "1", "totallyarealemail.google.com");
+
+    expect(employee2.getEmail()).toBeFalsy();
+});
