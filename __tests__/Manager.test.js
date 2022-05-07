d9c4bea2f6d858624cd89e1d5e0d31b710d636cc
Jonathan Cushing <firex42069@gmail.com>
2022-05-07 13:49:08 -0400

    manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,2 +1,12 @@
 const Employee = require("../lib/Employee.js");
 const Manager = require("../lib/Manager.js");
+
+test("creates a manager object", () => {
+    const manager = new Manager("Jonathan", "4", "notarealemail@gmail.com", "42");
+
+    expect(manager.name).toBe("Jonathan");
+    expect(manager.id).toBe("4");
+    expect(manager.email).toBe("notarealemail@gmail.com");
+    expect(manager.officeNumber).toBe("42");
+    expect(manager.getRole()).toBe("Manager");
+});
